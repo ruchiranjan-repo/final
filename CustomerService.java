6ebed91eb27fb4cd4d40e86ef6d20d56a0e7db75
ruchiranjan-repo <62060004+ruchiranjan-repo@users.noreply.github.com>
2020-03-17 20:13:21 +0530

    customer

---
+++ CustomerService.java
@@ -0,0 +1,12 @@
+package com.demobank.accountmanagement.services;
+
+import com.demobank.accountmanagement.dtos.CustomerDto;
+import com.demobank.accountmanagement.exception.CustomerNotLoggedInException;
+import com.demobank.accountmanagement.exception.UserNotFoundException;
+
+public interface CustomerService {
+	public String customerLogin(String emailId, String password) throws UserNotFoundException;
+	public CustomerDto findCustomerByCustomerId(Long customerId) throws UserNotFoundException;
+	
+	public Boolean checkLoggingStatus(Long customerId) throws CustomerNotLoggedInException;
+}
